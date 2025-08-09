935d233aa2152d7223c706675ac1122cf21da030
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-08-10 00:20:24 +0800

    Testing the PR

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('includes/header.php');
 
 <h2>Welcome to our homepage!</h2>
 <p>We sell office, school, and sanitary supplies.</p>
-
+<p>Testing the PR!</p>
 <?php
 include('includes/footer.php');
  ?>
