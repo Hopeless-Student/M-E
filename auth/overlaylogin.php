e565d8e28ab001770e0282372ff2c544e2508062
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-08-22 21:45:32 +0800

    updated the flow of login

--- auth/overlaylogin.php
+++ auth/overlaylogin.php
@@ -1,40 +1,9 @@
 
 <?php
-  session_start();
-  include("..\includes\database.php");
-  $pdo = connect();
-  $loginFailed ="";
-  $showModal = false;
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        $sql = "SELECT id, users, password FROM users WHERE users = :username";
-        $stmt = $pdo->prepare($sql);
-        $stmt-> execute(["username"=>$username]);
-
-        $user = $stmt->fetch(PDO::FETCH_ASSOC); // nagiging assoc array siya
-        if($user){
-            if($user && password_verify($password, $user["password"])){
-              $_SESSION["id"] = $user["id"];
-              $_SESSION["user"] = $user["users"];
-              header("Location: ..\index.php");
-              exit();
-            } else {
-              $loginFailed = "Incorrect password";
-              $showModal = true;
-            }
-        } else {
-            $loginFailed = "User not found";
-            $showModal = true;
-        }
-      }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Login Modal</title>
+  include("login_handler.php");
+  if (!isset($loginFailed)) $loginFailed = "";
+  if (!isset($showModal)) $showModal = false;
+ ?>
   <style>
     * {
       box-sizing: border-box;
@@ -176,6 +145,3 @@
         });
       <?php endif; ?>
   </script>
-
-</body>
-</html>
--- index.php
+++ index.php
@@ -1025,3 +1025,25 @@
 <?php
 include('includes/footer.php');
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    if (!isset($_SESSION["user"])) {
+        echo "Browse all you want! <br>";
+    } else {
+      echo "Hello, {$_SESSION['user']} <br>";
+      echo "<form class='logoutbtn' action='auth/logout.php' method='post'>";
+      echo "<input type='submit' name='logout' value='Log out'>";
+      echo "</form>";
+    }
+     ?>
+     <button type="button" name="button" style="width: 60px; height: 20px;">Item 1</button>
+     <button type="button" name="button" style="width: 60px; height: 20px;">Item 2</button>
+     <button type="button" name="button" style="width: 60px; height: 20px;">Item 3</button>
+  </body>
+</html>
