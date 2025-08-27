0d9f577c75be9c620bbd06582cf2d2591aeeaae6
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-08-27 17:46:34 +0800

    uploaded m_e.sql to database folder

--- test.php
+++ test.php
@@ -1,15 +1,14 @@
 <?php
+// Correct the path to the cacert.pem file
+$cacert_path = "C:/xampp/php/cacert.pem";  // Update this path accordingly
 
-require 'vendor/autoload.php';
+// Check OpenSSL version
+echo "OpenSSL version: " . OPENSSL_VERSION_TEXT . "\n";
 
-use Dotenv\Dotenv;
-echo '<pre>';
-var_dump(class_exists('Dotenv\Dotenv'));  // Should print true if Dotenv is loaded
-echo '</pre>';
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-echo getenv('APP_NAME');  // Or any environment variable defined in your .env file
-
- ?>
+// Check if the cacert.pem file can be loaded
+if (file_get_contents($cacert_path)) {
+    echo "cacert.pem is being loaded correctly.\n";
+} else {
+    echo "Error loading cacert.pem.\n";
+}
+?>
