946d8235a4136699dfe71562cd70682419583cff
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-08-31 01:43:55 +0800

    added user-sidebar.php&css/starter bootstrap profile layout

---
+++ includes/user-sidebar.php
@@ -0,0 +1,15 @@
+  <aside class="sidebar">
+      <div class="profile">
+          <img src="../assets/images/reirei.jpg" alt="User Avatar" class="avatar">
+          <h2 class="username">Naoi Rei</h2>
+          <p class="role">Idol</p>
+      </div>
+      <nav class="sidebar-nav">
+          <ul>
+              <li><a href="dashboard.php">Dashboard</a></li>
+              <li><a href="profile.php">Profile</a></li>
+              <li><a href="edit-profile.php">Settings</a></li>
+              <li><a href="../auth/logout.php">Logout</a></li>
+          </ul>
+      </nav>
+  </aside>
---
+++ user/profile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="../assets/css/user-sidebar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+    crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include('../includes/user-sidebar.php'); ?>
+    <div class="main-content">
+      <h1>Welcome to your Dashboard</h1>
+      <p>Here is some content.</p>
+    </div>
+
+
+  </body>
+</html>
