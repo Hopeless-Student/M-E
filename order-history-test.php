6a1c45c9135dc4e2ad86984c21450357133d9209
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-09-17 11:20:18 +0800

    added bootstrap files for UI

--- order-history-test.php
+++ order-history-test.php
@@ -31,8 +31,7 @@ try {
 <html>
 <head>
     <title>My Cart</title>
-    <!-- Bootstrap 5 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap-5.3.8-dist/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
     <div class="container my-5">
--- user/profile.php
+++ user/profile.php
@@ -6,9 +6,7 @@
   <head>
     <meta charset="utf-8">
     <title>Edit User Profile</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
-    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
-    crossorigin="anonymous">
+    <link href="../bootstrap-5.3.8-dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/user-sidebar.css">
   </head>
   <body>
@@ -145,9 +143,12 @@
         </form>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    <!-- <script src="bootstrap-5.3.8-dist\js\bootstrap.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous"></script> -->
+    <script src="../bootstrap-5.3.8-dist/js/bootstrap.min.js">
+
+    </script>
 
     <script>
 
