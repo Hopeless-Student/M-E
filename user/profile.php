c8cf8bd9ce4403b4ae4e4eb5c653347e273c3065
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-09-17 11:20:18 +0800

    added bootstrap files for UI

--- user/profile.php
+++ user/profile.php
@@ -198,7 +198,10 @@ $barangays = $pdo->query("SELECT barangay_id, barangay_name, city_id FROM barang
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous"></script> -->
+    <script src="../bootstrap-5.3.8-dist/js/bootstrap.min.js">
+
+    </script>
 
     <script>
 
