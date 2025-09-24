a8148cdeefa56254308af4d1769f0ada6bf8b6de
Cjay Gonzales <cjaygonzales1006@gmail.com>
2025-09-24 17:51:10 +0800

    completed the user update profile

--- pages/about.php
+++ pages/about.php
@@ -5,11 +5,23 @@
   <head>
     <meta charset="utf-8">
     <title>About us</title>
-    <link rel="stylesheet" href="../assets/css/navbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../test-files/homepage.css">
   </head>
   <body>
-    <?php include('../includes/navbar.php') ?>
-    <h1>About M&E Interior Supplies Trading</h1>
-  </body>
-  <?php include('../includes/footer.php') ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-royal px-4 py-2">
+      <a class="navbar-brand fw-bold" href="#"><img src="../assets/images/M&E_LOGO_transparent.png" class="img-fluid" alt="M&E Logo"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link" href="../test-files/testing2.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="order_form.php">Products</a></li>
+          <li class="nav-item"><a class="nav-link active" href="about.php">About</a></li>
+        </ul>
+        <a href="#" class="btn btn-outline-light me-2"><i class="bi bi-cart"></i> Cart</a>
+        <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+      </div>
+    </nav>
 </html>
