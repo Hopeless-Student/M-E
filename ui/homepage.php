f1764846ab5c1dc3911adb6e1f4c8f1d0ac5b8c3
Ace <pajmasinsin12@gmaill.com>
2025-09-28 15:46:41 +0800

    made the footer reusable with PHP

--- ui/homepage.php
+++ ui/homepage.php
@@ -426,62 +426,11 @@
 
 
 
-        <footer>
-            <div class="footer-container">
-                <div class="footer-brand">
-                    <div class="brand-top">
-                        <img alt="M&E Logo" class="footer-logo" src="../assets/images/M&E_LOGO-semi-transparent.png"/>
-                        <h3>Interior Supplies Trading</h3>
-                    </div>
-                </div>
+        <?php
+            include '../includes/footer.php';
+            renderFooter();
+        ?>
 
-                <div class="footer-links">
-                    <div class="footer-column">
-                        <h4>Navigation</h4>
-                        <ul>
-                            <li><a href="#">School</a></li>
-                            <li><a href="#">Office</a></li>
-                            <li><a href="#">Sanitary</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="footer-column">
-                        <h4>Customer Support</h4>
-                        <ul>
-                            <li><a href="#">FAQs</a></li>
-                            <li><a href="#">Shipping</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="footer-column">
-                        <h4>Company Info</h4>
-                        <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Our Store</a></li>
-                            <li><a href="#">Bulk Orders</a></li>
-                            <li><a href="#">Return & Refund Policy</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <hr class="footer-divider"/>
-
-            <div class="footer-socials">
-                <h4>Stay Connected</h4>
-                <p>Follow us for the latest deals & updates!</p>
-
-                <a class="fb-btn" href="https://www.facebook.com/youronlineshooping" target="_blank">
-                    <img alt="Facebook Link" src="../assets/images/Facebook_Logo_2023.png"/>
-                    <span>Follow us on Facebook</span>
-                </a>
-            </div>
-
-            <div class="footer-copy">
-                <p>© Copyright 2025 M&E Interior Supplies Trading, All rights reserved.</p>
-            </div>
-        </footer>
 
 
 
@@ -579,7 +528,7 @@
                 </ol>
             </div>
         </div>
-        
+
         <script src="../bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
         <script src="../bootstrap-5.3.8-dist/js/bootstrap.bundle.js"></script>
         <script src="../assets/js/homepage.js"></script>
